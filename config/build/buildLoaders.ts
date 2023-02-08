4089490b05a221c6d67a4f73dde083b6e27be8e1
Dmitry Korovin <d.korovin@alt-it.solutions>
2023-02-08 09:33:45 +0300

    feat: add file loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,6 +9,15 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 		use: ['@svgr/webpack'],
 	};
 
+	const fileLoader = {
+		test: /\.(png|jpe?g|gif)$/i,
+		use: [
+			{
+				loader: 'file-loader',
+			},
+		],
+	};
+
 	const cssLoader = {
 		test: /\.s[ac]ss$/i,
 		use: [
@@ -34,6 +43,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 
 	return [
 		svgLoader,
+		fileLoader,
 		typescriptLoader,
 		cssLoader,
 	]
--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -3,6 +3,10 @@ declare module '*.scss' {
 	export default content;
 }
 
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
+
 declare module "*.svg" {
 	const content: React.VFC<React.SVGProps<SVGSVGElement>>;
 	export default content;

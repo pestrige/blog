2aa307142ba380707531e5d3d6018027a015e752
Dmitry Korovin <d.korovin@alt-it.solutions>
2023-02-12 12:49:02 +0300

    feat: add jest and first unit test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,19 @@
+import { classNames } from "./classNames";
+
+describe("classNames", () => {
+	test("with only one argument", () => {
+		expect(classNames("someClass")).toBe("someClass");
+	});
+	test("with a few string arguments", () => {
+		const args = "someClass someClass1 someClass2";
+		expect(classNames(args)).toBe(args);
+	});
+	test("with a argument as object", () => {
+		const expected = "someClass someClass1";
+		expect(classNames("someClass", { someClass1: true, someClass2: false })).toBe(expected);
+	});
+	test("with an undefined argument", () => {
+		const expected = "someClass someClass1";
+		expect(classNames("someClass", undefined, { someClass1: true, someClass2: false })).toBe(expected);
+	});
+});

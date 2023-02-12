8f437dbdb2af4058c3e080b670f36e23d7a7b67f
Dmitry Korovin <d.korovin@alt-it.solutions>
2023-02-12 08:45:03 +0300

    feat: add hot reload

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,6 +6,7 @@ export function buildDevServer(options: BuildOptions): DevServerConfiguration {
 		port: options.port,
 		open: true,
 		historyApiFallback: true,
+		hot: true,
 	}
 }
 
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,16 +1,24 @@
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BuildOptions } from "./types/config";
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-	return [
+	const plugins = [
 		new webpack.ProgressPlugin(),
 		new HtmlWebpackPlugin({ template: paths.html }),
 		new MiniCssExtractPlugin({
 			filename: "css/[name].[contenthash:8].css",
 			chunkFilename: "css/[name].[contenthash:8].css"
 		}),
-		new webpack.DefinePlugin({ __IS_DEV__: isDev })
-	]
+		new webpack.DefinePlugin({ __IS_DEV__: isDev }),
+	];
+
+	if (isDev) {
+		plugins.push(new webpack.HotModuleReplacementPlugin());
+		plugins.push(new ReactRefreshWebpackPlugin());
+	}
+
+	return plugins;
 }

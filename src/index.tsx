430143fd23628814fc0763e7f7f792beb2a11166
Dmitry Korovin <d.korovin@alt-it.solutions>
2023-04-21 15:47:53 +0300

    feat: migrate to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import { App } from "app/App";
 import { StoreProvider, ThemeProvider } from "app/providers";
-
 import "./shared/config/i18n";
 
-render(
+const container = document.getElementById("root");
+if (!container) {
+	throw new Error("Контейнер root не найден. Не удалось вмонтировать React приложение.");
+}
+
+const root = createRoot(container);
+root.render(
 	<BrowserRouter>
 		<StoreProvider>
 			<ThemeProvider>
 				<App />
 			</ThemeProvider>
 		</StoreProvider>
-	</BrowserRouter>,
-	document.getElementById("root")
+	</BrowserRouter>
 );

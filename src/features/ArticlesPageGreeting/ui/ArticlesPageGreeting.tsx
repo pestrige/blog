5916efc453701924682482feb3da30777d9e1726
Dmitry Korovin <d.korovin@alt-it.solutions>
2023-06-20 03:51:37 +0300

    fix: add redesigned article page greeting

--- src/features/ArticlesPageGreeting/ui/ArticlesPageGreeting.tsx
+++ src/features/ArticlesPageGreeting/ui/ArticlesPageGreeting.tsx
@@ -1,17 +1,24 @@
 import { memo } from "react";
 import { useTranslation } from "react-i18next";
-import { Modal, TextDeprecated } from "@/shared/ui";
+import { Modal, Text, TextDeprecated } from "@/shared/ui";
+import { ToggleFeaturesWrapper } from "@/shared/lib";
 import { useArticlesPageGreeting } from "../hooks/useArticlesPageGreeting";
 
 export const ArticlesPageGreeting = memo((): JSX.Element => {
 	const { t } = useTranslation("articles");
 	const { isOpen, onClose } = useArticlesPageGreeting();
 
+	const textProps = {
+		title: t("Добро подаловать на страницу статей"),
+		text: t("Здесь вы можете искать и просматривать статьи на различные темы"),
+	};
+
 	return (
 		<Modal isOpen={isOpen} onClose={onClose}>
-			<TextDeprecated
-				title={t("Добро подаловать на страницу статей")}
-				text={t("Здесь вы можете искать и просматривать статьи на различные темы")}
+			<ToggleFeaturesWrapper
+				featureName="isAppRedesigned"
+				on={<Text {...textProps} />}
+				off={<TextDeprecated {...textProps} />}
 			/>
 		</Modal>
 	);
